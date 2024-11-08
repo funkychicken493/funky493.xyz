13f2e97cd8d97717b5236b327f3bafa8bae51586
funkychicken493 <82616867+funkychicken493@users.noreply.github.com>
2024-11-08 11:04:47 -0600

    header

--- header.js
+++ header.js
@@ -1,1 +1,23 @@
-fetch("header.html").then(response => response.text()).then(text => document.getElementById("header").innerHTML = text);
+let header = `
+    <head>
+    <title>what</title>
+    <link rel="stylesheet" href="styles.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" 
+        content="width=device-width, initial-scale=1.0">
+    </head>
+
+    <header>
+        <div class="nav-bar">
+            <div id="name"><a href="index.html">funky493.xyz</a></div>
+            <nav>
+                <ul class="nav-links">
+                    <li><a href="index.html">home</a></li>
+                    <li><a href="#">about</a></li>
+                    <li><a href="#">stuff</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+`;
+document.getElementById("header").innerHTML = header;
